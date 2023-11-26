cde8e6939ca19ddf739fc458d28730c90f354f5e
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2023-11-25 21:00:07 -0800

    Centered Nuisance Reports and created table component

--- NCTPortalApp/src/app/app.module.ts
+++ NCTPortalApp/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ReportComponent } from './report/report.component';
 import { ReportListComponent } from './report-list/report-list.component';
 import { RectangleContainerComponent } from './rectangle-container/rectangle-container.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReportComponent,
     ReportListComponent,
-    RectangleContainerComponent
+    RectangleContainerComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
