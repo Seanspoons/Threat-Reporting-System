3e2eb7bb9f9a70181993b3d960045d3ef28b8a83
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2023-11-27 20:35:55 -0800

    Created routing module

--- NCTPortalApp/src/app/app.module.ts
+++ NCTPortalApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SearchPipe } from './pipes/search.pipe';
 import { ReportAddFormComponent } from './components/report-add-form/report-add-form.component';
 import { MapComponent } from './components/map/map.component';
 import { MoreInfoComponent } from './components/more-info/more-info.component';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { MoreInfoComponent } from './components/more-info/more-info.component';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
