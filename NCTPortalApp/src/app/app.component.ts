6f85ea3a3db15a9af9bbd0a7acb6fbf279678987
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2023-11-29 09:48:06 -0800

    Created map button that dynamically is available depending on page

--- NCTPortalApp/src/app/app.component.ts
+++ NCTPortalApp/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The Night Crusade Titans Portal';
+  isOnMainRouteValue: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.checkRoute();
+    // Subscribe to router events
+    this.router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.checkRoute();
+      });
+  }
+
+  checkRoute() {
+    this.isOnMainRouteValue = this.router.url === '/rectangle-container';
+  }
+
+  isOnMainRoute(): boolean {
+    return this.isOnMainRouteValue;
+  }
 }
