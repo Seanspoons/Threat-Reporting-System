bafc91d1a506443f7fcc3a1dd7869be10927d402
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2023-11-29 18:33:21 -0800

    Implemented basic leaflet map

--- NCTPortalApp/src/app/components/map/map.component.ts
+++ NCTPortalApp/src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -10,6 +11,11 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var mymap = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(mymap);
   }
 
 }
