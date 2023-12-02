148e6534253c139935508c48f350f1ea72c0049e
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2023-12-01 16:02:35 -0800

    Fix: Removed print statements

--- NCTPortalApp/src/app/services/report-service.service.ts
+++ NCTPortalApp/src/app/services/report-service.service.ts
@@ -20,8 +20,7 @@ export class ReportServiceService{
     this.locations = [];
   }
 
-  get() { // I think it is going through this everytime there is an addition too. Which ends up creating too many elements in the table. Possibly look at how I might be pushing onto array in the add already
-    console.log("It is first load: " + this.firstLoad);
+  get() {
     if(this.firstLoad && this.reports.length !== 1) {
       this.http.get('https://272.selfip.net/apps/22m6j5mz3y/collections/reports/documents/')
       .subscribe((data)=>{
@@ -31,7 +30,6 @@ export class ReportServiceService{
           newReport.id = rows[i].data.id;
           newReport.date = rows[i].data.date;
           newReport.status = rows[i].data.status;
-          console.log("Pushing into reports from get");
           this.reports.push(newReport);
           this.getLocations();
           this.firstLoad = false;
@@ -44,13 +42,11 @@ export class ReportServiceService{
   }
 
   add(newReport: NuisanceReport) {
-    console.log("It is first load: " + this.firstLoad);
     this.http.post<NuisanceReport>('https://272.selfip.net/apps/22m6j5mz3y/collections/reports/documents/', {
       "key": newReport.id,
       "data": newReport
     }).subscribe(
       (data: any) => {
-        console.log("Pushing into reports from add");
         this.reports.push(newReport);
         this.getLocations();
         this.firstLoad = false;
